c8bef6c52ef0a7ab9ade7b9c9cb5632ebb5b1640
Mukesh G Chandnani <mukesh51@gmail.com>
2022-09-29 00:24:39 +0100

    Added greet method

--- com/test/Main.java
+++ com/test/Main.java
@@ -3,6 +3,12 @@ package com.test;
 public class Main {
     public static void main(String args[]) {
        System.out.println("Hello World");
+       System.out.println(greet());
 }
+
+public String greet() {
+    return "How are you";
+}
+
 }
 
