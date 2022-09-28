9f99ae88f7c03d1404c50e2f71b742a090057ad1
Aarti's Mac <aarti@Aartiss-MacBook-Air.local>
2022-09-29 00:28:00 +0100

    Added new method

--- com/test/Main.java
+++ com/test/Main.java
@@ -4,10 +4,15 @@ public class Main {
     public static void main(String args[]) {
        System.out.println("Hello World");
        System.out.println(greet());
+       System.out.println(greetUser("Aarti"));
 }
 
 public String greet() {
     return "How are you";
+}
+
+public String greetUser(String name) {
+    return "How are you" + name;
 }
 
 }
